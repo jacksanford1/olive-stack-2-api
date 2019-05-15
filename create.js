a93040e708b9e4c5646afa5ac307252fd06b9cdc
john sanford <Jack@Macintosh.local>
2019-05-14 20:32:59 -0700

    Making a mistake

--- create.js
+++ create.js
@@ -3,7 +3,8 @@ import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-  const data = JSON.parse(event.body);
+  gibberish.what;
+const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
     Item: {
